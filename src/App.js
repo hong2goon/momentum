2f73cc9dad1c585310aab84bf29caf8f7222c898
hong2goon <hong2goon@email.com>
2023-03-17 17:52:59 +0900

    region.flex 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ function App() {
 
   return (
     <div className="App">
+      <div className="region flex">
+        <div className="flex-item half-top">
+          시계, 인사말 들어갈 자리
+        </div>
+        <div className="flex-item half-bottom">
+          투두리스트 들어갈 자리
+        </div>
+      </div>
       <Background />
     </div>
   );
